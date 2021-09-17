d10cb66d3391db5cb73d23397e052826c4b7d3ad
ingeomar2001 <89227568+ingeomar2001@users.noreply.github.com>
2021-09-16 20:19:38 -0500

    Create ScriptTablaCliente.sql

---
+++ Semana 5/ScriptTablaCliente.sql
@@ -0,0 +1,7 @@
+use erp;
+CREATE TABLE `cliente` (
+  `ID` int NOT NULL AUTO_INCREMENT,
+  `Nombre` varchar(45) DEFAULT NULL,
+  `Direccion` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
